ab2f3ce88c9c44d198e9e06c527ab499aa7ac5b2
Nicholas Degrate <nicholasdegrate@gmail.com>
2021-01-29 01:52:55 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,1 +1,36 @@
 # Write your code here!
+=begin
+  top level hash 
+    -> :home
+      -> :team_name - String
+      -> :colors - array[string]
+      -> :players - [{
+        
+      }]
+    -> :away
+=end
+game_hash = {
+  :home => å {
+    :team_name => "Brooklyn Nets",
+    :colors => ["Black", "White"],
+    :players => [
+      {
+        :player_name => "Alan Anderson",
+        :number
+:shoe
+:points
+:rebounds
+:assists
+:steals
+:blocks
+:slam_dunks
+        
+      }
+    ],
+
+  },
+}
+// declaring method
+def game_hash
+  
+end
